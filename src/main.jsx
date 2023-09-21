02e485a8418e7eccfb7d854721ad1420cc41b59e
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-09-22 01:31:34 +0600

    adding root file and website logo and name

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <h1>hello world</h1>
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
