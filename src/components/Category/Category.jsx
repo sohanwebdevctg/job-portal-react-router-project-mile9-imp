4cf614bfd4a83056830e81414c4c4c364f995027
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-09-26 02:46:34 +0600

    responsive root page

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -11,7 +11,7 @@ const Category = ({category}) => {
   // eslint-disable-next-line react/prop-types
   const {logo,category_name,availability} = category;
   return (
-    <div className='bg-gray-400 md:p-6 py-3 md:py-0 mx-3 md:mx-0 text-center md:text-left rounded shadow-lg shadow-blue-500'>
+    <div className='bg-gray-400 py-3 md:py-6 md:px-4 mx-3 md:mx-0 text-center md:text-left rounded shadow-lg shadow-blue-500'>
       {navigation.state === 'loading' ? <Loading></Loading> : ''}
       {/* category section start */}
       <img className='shadow-md p-2 text-center md:text-left inline-block md:inline' src={logo}></img>
