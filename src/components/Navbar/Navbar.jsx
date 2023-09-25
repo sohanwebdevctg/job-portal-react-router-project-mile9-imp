6b2200b02f7659fd614d6fa1ba3aae09c59f9ede
sohanwebdevctg <amirhossainsohan0@gmail.com>
2023-09-26 01:35:03 +0600

    navbar mobile responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
   const [showBtn, setShowBtn] = useState(false)
@@ -27,23 +28,23 @@ const Navbar = () => {
       {/* desktop section end */}
       {/* mobile section start */}
       <div className='bg-blue-700'>
-        <nav className='flex justify-between items-center h-20'>
+        <nav className='flex  justify-between items-center h-24 mx-2'>
           <div>
               <NavLink to="/">
-              <h1 className='text-slate-300 font-bold text-lg'><i>JobPortal</i></h1>
+              <h1 className='text-slate-300 font-bold text-xl'><i>JobPortal</i></h1>
               </NavLink>
           </div>
           <div>
             {
-              showBtn ? (<span onClick={() => setShowBtn(false)} className='text-white font-semibold'>hide</span>) : (<span onClick={() => setShowBtn(true)}className='text-white font-semibold'>show</span>)
+              showBtn ? (<span onClick={() => setShowBtn(false)} className='text-white font-semibold'><XMarkIcon className="h-6 w-6 text-slate-300" /></span>) : (<span onClick={() => setShowBtn(true)}className='text-white font-semibold'><Bars3Icon className="h-6 w-6 text-slate-300" /></span>)
             }
           </div>
         </nav>
-        <div className={`absolute w-full ${showBtn ? 'top-16' : '-top-48'} duration-700 bg-blue-700`}>
-            <NavLink to="/" className={({isActive}) => isActive ? 'text-gray-400 font-semibold' : 'text-white font-semibold'}>Home</NavLink>
-            <NavLink to="/appliedJobs" className={({isActive}) => isActive ? 'text-gray-400 font-semibold' : 'text-white font-semibold'}>Applied Jobs</NavLink>
-            <NavLink to="/statistics" className={({isActive}) => isActive ? 'text-gray-400 font-semibold' : 'text-white font-semibold'}>Statistics</NavLink>
-            <NavLink to="/blog" className={({isActive}) => isActive ? 'text-gray-400 font-semibold' : 'text-white font-semibold'}>Blog</NavLink>
+        <div className={`absolute w-full ${showBtn ? 'top-24' : '-top-52'} duration-700 bg-blue-700`}>
+            <NavLink to="/" className={({isActive}) => isActive ? 'text-gray-400 font-semibold block text-center py-3' : 'text-white font-semibold block text-center py-3'}>Home</NavLink>
+            <NavLink to="/appliedJobs" className={({isActive}) => isActive ? 'text-gray-400 font-semibold block text-center py-3' : 'text-white font-semibold block text-center py-3'}>Applied Jobs</NavLink>
+            <NavLink to="/statistics" className={({isActive}) => isActive ? 'text-gray-400 font-semibold block text-center py-3' : 'text-white font-semibold block text-center py-3'}>Statistics</NavLink>
+            <NavLink to="/blog" className={({isActive}) => isActive ? 'text-gray-400 font-semibold block text-center py-3' : 'text-white font-semibold block text-center py-3'}>Blog</NavLink>
         </div>
       </div>
       {/* mobile section end */}
